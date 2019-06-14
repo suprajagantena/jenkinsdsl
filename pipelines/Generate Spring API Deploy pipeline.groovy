22bce7400d62a9f922b64260d8d78de5499021fb
suprajagantena <suprajagantena@gmail.com>
2019-06-14 16:34:38 -0400

    Create Generate Spring API Deploy pipeline.groovy

---
+++ pipelines/Generate Spring API Deploy pipeline.groovy
@@ -0,0 +1,6 @@
+node(){
+ 
+    stage('project checkout') {
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/DevopsMG/SpringProjectforCICD']]])
+    }
+}
